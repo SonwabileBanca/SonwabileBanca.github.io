5c69557f3a2eb858939e79e6d918d61ac4b34058
Sonwabile Banca <135272675+SonwabileBanca@users.noreply.github.com>
2023-11-23 09:30:10 +0200

    Update finance_calculators.py
    Added documentation and improved the display of the function

--- finance_calculators.py
+++ finance_calculators.py
@@ -1,3 +1,20 @@
+'''Standardized Input Checking:
+
+Used .lower() method to convert the user's input to lowercase. This ensures that the program works regardless of the user's choice being in uppercase, lowercase, or a combination.
+Added Comments:
+
+Added comments to explain each section of the code, including the purpose of the program, user prompts, input variables, and the logic behind each calculation.
+Improved Variable Naming:
+
+Renamed the variables inside the if conditions to reflect the nature of the calculated values. For example, simple_interest, compound_interest, and bond_repayment make it clearer what each variable represents.
+Standardized Choice Comparisons:
+
+Used investorChoice.lower() == "simple" instead of multiple conditions to make the code cleaner and more readable.
+Remember, good documentation and clear variable naming are essential for making your code understandable to others (or even to yourself in the future).'''
+
+#=================================Financial Calculator For interest and Bonds==================================
+
+#=======================================Simple==========Compound============Bond=============================
 import math
 #Display the Menu to user
 print('Investment - to calculate the amount of interest you will earn on your investment\n\nBond - calculate the amount you will have to pay on a home loan.\n\nEnter either Investment or bond\n\n\t')
