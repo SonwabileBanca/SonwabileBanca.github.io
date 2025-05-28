9fe936147ba143fa2913c75d8f552cb54f769db4
Sonwabile Banca <135272675+SonwabileBanca@users.noreply.github.com>
2025-05-28 12:32:47 +0200

    Data Structures

---
+++ studentID.java
@@ -0,0 +1,14 @@
+public class studentID {
+    public static void main(String[] args) {
+        
+        //Declare and initialise arrays.
+        int [] studentid = {
+            1123231, 1125554, 121112
+        };
+        
+        int value = studentid[0];
+        System.out.println("The Student Index is at:");
+        System.out.print(value);
+    }
+    
+}
